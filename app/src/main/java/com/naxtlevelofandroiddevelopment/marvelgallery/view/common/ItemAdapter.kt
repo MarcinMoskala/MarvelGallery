ea9d6a5eec826fbcfbf9c300c0391071044007e0
MarcinMoskala <marcinmoskala@gmail.com>
2017-08-07 20:49:57 +0200

    Simplify item adapter

--- app/src/main/java/com/naxtlevelofandroiddevelopment/marvelgallery/view/common/ItemAdapter.kt
+++ app/src/main/java/com/naxtlevelofandroiddevelopment/marvelgallery/view/common/ItemAdapter.kt
@@ -6,15 +6,12 @@ import android.view.View
 
 abstract class ItemAdapter<T : RecyclerView.ViewHolder>(@LayoutRes open val layoutId: Int) {
 
-    protected var holder: T? = null
-
     abstract fun onCreateViewHolder(itemView: View): T
 
     @Suppress("UNCHECKED_CAST")
     fun bindBaseViewHolder(holder: RecyclerView.ViewHolder) {
-        this.holder = holder as T
-        holder.onBindViewHolder()
+        (holder as T).onBindViewHolder()
     }
 
-    abstract fun T.onBindViewHolder()
+    abstract protected fun T.onBindViewHolder()
 }
